41196fa3552fcda712c1ad2761978d6ce45d7ef4
maljuburi <maljuburi89@gmail.com>
2019-02-16 09:53:40 -0700

    init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import io from "socket.io-client";
+import "./App.css";
 
 class App extends Component {
+  componentDidMount() {
+    this.initSocket();
+  }
+
+  initSocket = () => {
+    const socket = io.connect("http://localhost:5000");
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="container">
+        <h1>Progress tracker</h1>
       </div>
     );
   }
